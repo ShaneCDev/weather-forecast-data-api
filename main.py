99ebd4b2a3176fa6aed4e990f730721af2ceee61
Shane <shane.conway@nvd.ie>
2024-02-23 14:46:45 +0000

    Fixed the temperatures displayed and handled the KeyError exception.

--- main.py
+++ main.py
@@ -11,22 +11,24 @@ option = st.selectbox("Select data to view",
                       ("Temperature", "Sky"))
 st.subheader(f'{option} for the next {days} days in {place}')
 
+
 if place:
     # Get temp/sky data
-    filtered_data = get_data(place, days)
+    try:
+        filtered_data = get_data(place, days)
 
-    if option == "Temperature":
-        temperatures = [dict['main']['temp'] for dict in filtered_data]
-        new_temps = [new_temps / 10 for new_temps in temperatures]
-        final_temp = []
-        dates = [dict['dt_txt'] for dict in filtered_data]
-        # Create a temperature plot
-        figure = px.line(x=dates, y=new_temps, labels={"x": "Date", "y": "Temperature (C)"})
-        st.plotly_chart(figure)
+        if option == "Temperature":
+            temperatures = [dict['main']['temp'] / 10 for dict in filtered_data]
+            dates = [dict['dt_txt'] for dict in filtered_data]
+            # Create a temperature plot
+            figure = px.line(x=dates, y=temperatures, labels={"x": "Date", "y": "Temperature (C)"})
+            st.plotly_chart(figure)
 
-    if option == "Sky":
-        images = {"Clear": "images/clear.png", "Clouds": "images/cloud.png",
-                  "Rain": "images/rain.png", "Snow": "images/snow.png"}
-        sky_conditions = [dict['weather'][0]['main'] for dict in filtered_data]
-        image_paths = [images[condition] for condition in sky_conditions]
-        st.image(image_paths, width=115)
+        if option == "Sky":
+            images = {"Clear": "images/clear.png", "Clouds": "images/cloud.png",
+                      "Rain": "images/rain.png", "Snow": "images/snow.png"}
+            sky_conditions = [dict['weather'][0]['main'] for dict in filtered_data]
+            image_paths = [images[condition] for condition in sky_conditions]
+            st.image(image_paths, width=115)
+    except KeyError:
+        st.write("This place does not exist.")
